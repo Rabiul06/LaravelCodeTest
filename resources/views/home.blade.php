22b3d23a9297ed9f5fd898daeabf5b7fd402b5ed
Rabiul isalm <robecse99@gmail.com>
2021-04-03 10:40:12 +0600

    CodeTest

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
+                    
                     {{ __('You are logged in!') }}
                 </div>
             </div>
